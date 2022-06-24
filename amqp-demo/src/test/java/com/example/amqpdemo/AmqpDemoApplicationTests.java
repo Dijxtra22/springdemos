07dfd68cc1b45dcbe232714b306a7515f67ba60a
Echoes_ <lsywangyi626@163.com>
2022-06-24 15:02:52 +0800

    -

---
+++ amqp-demo/src/test/java/com/example/amqpdemo/AmqpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amqpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
